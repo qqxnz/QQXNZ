78f2ea3aa603a827a7725d2eeea6b26e3a2ded9d
qqxnz <qq89232369@163.com>
2018-03-22 09:36:55 +0800

    1.0
    1.0

---
+++ QQXNZ/NZ_Data.m
@@ -0,0 +1,171 @@
+//
+//  NZ_Data.m
+//  QQXNZTest
+//
+//  Created by mm on 2017/8/21.
+//  Copyright © 2017年 mm. All rights reserved.
+//
+
+#import "NZ_Data.h"
+#import "CommonCrypto/CommonDigest.h"
+
+
+@implementation NZ_Data
+
+
++ (NSDictionary *)dictionaryWithJsonString:(NSString *)jsonString {
+    
+    if (jsonString == nil) {
+        
+        return nil;
+        
+    }
+    
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSError *err;
+    
+    NSDictionary *dic = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:&err];
+    
+    if(err) {
+        
+        NSLog(@"json解析失败：%@",err);
+        
+        return nil;
+        
+    }
+    
+    return dic;
+    
+}
+
+
++ (NSString*)dictionaryToJson:(NSDictionary *)dic{
+    
+    NSError *parseError = nil;
+    
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dic options:NSJSONWritingPrettyPrinted error:&parseError];
+    
+    return [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    
+}
+
+
+#pragma mark -- DataToBase64
++ (NSString *)base64EncodeWithData:(NSData *)sourceData {
+    if (!sourceData) {
+        return nil;
+    }
+    NSString *resultStr = [sourceData base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    return resultStr;
+}
+
+#pragma mark -- Base64ToData
++ (NSData *)base64DecodeWithString:(NSString *)sourceString {
+    if (!sourceString) {
+        return nil;
+    }
+    // 解密
+    NSData *resultData = [[NSData alloc] initWithBase64EncodedString:sourceString options:NSDataBase64DecodingIgnoreUnknownCharacters];
+    //    NSLog(@"%@",resultData);
+    return resultData;
+}
+
++(NSString *)md5DigestWithString:(NSString*)input{
+    const char* str = [input UTF8String];
+    unsigned char result[16];
+    CC_MD5(str, (uint32_t)strlen(str), result);
+    NSMutableString *ret = [NSMutableString stringWithCapacity:16 * 2];
+    for(int i = 0; i<16; i++) {
+        [ret appendFormat:@"%02x",(unsigned int)(result[i])];
+    }
+    return ret;
+}
+
++(NSString *)md5DigestWithData:(NSData*)input{
+    const char* str = [input bytes];
+    unsigned char result[16];
+    CC_MD5(str, (uint32_t)strlen(str), result);
+    NSMutableString *ret = [NSMutableString stringWithCapacity:16 * 2];
+    for(int i = 0; i<16; i++) {
+        [ret appendFormat:@"%02x",(unsigned int)(result[i])];
+    }
+    return ret;
+}
+
+
+
++(NSString*) sha1:(NSString *) string{
+    const char *cstr = [string cStringUsingEncoding:NSUTF8StringEncoding];
+    
+    NSData *data = [NSData dataWithBytes:cstr length:string.length];
+    
+    uint8_t digest[CC_SHA1_DIGEST_LENGTH];
+    
+    CC_SHA1(data.bytes, data.length, digest);
+    
+    NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
+    
+    for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++)
+        [output appendFormat:@"%02x", digest[i]];
+    
+    return output;
+}
+
+
+#pragma mark -- hex->data
++(NSData *)convertHexStrToData:(NSString *)str {
+    if (!str || [str length] == 0) {
+        return nil;
+    }
+    
+    NSMutableData *hexData = [[NSMutableData alloc] initWithCapacity:8];
+    NSRange range;
+    if ([str length] % 2 == 0) {
+        range = NSMakeRange(0, 2);
+    } else {
+        range = NSMakeRange(0, 1);
+    }
+    for (NSInteger i = range.location; i < [str length]; i += 2) {
+        unsigned int anInt;
+        NSString *hexCharStr = [str substringWithRange:range];
+        NSScanner *scanner = [[NSScanner alloc] initWithString:hexCharStr];
+        
+        [scanner scanHexInt:&anInt];
+        NSData *entity = [[NSData alloc] initWithBytes:&anInt length:1];
+        [hexData appendData:entity];
+        
+        range.location += range.length;
+        range.length = 2;
+    }
+    
+    NSLog(@"hexdata: %@", hexData);
+    return hexData;
+}
+
+#pragma mark -- data->hex
++(NSString *)convertDataToHexStr:(NSData *)data {
+    if (!data || [data length] == 0) {
+        return @"";
+    }
+    NSMutableString *string = [[NSMutableString alloc] initWithCapacity:[data length]];
+    
+    [data enumerateByteRangesUsingBlock:^(const void *bytes, NSRange byteRange, BOOL *stop) {
+        unsigned char *dataBytes = (unsigned char*)bytes;
+        for (NSInteger i = 0; i < byteRange.length; i++) {
+            NSString *hexStr = [NSString stringWithFormat:@"%x", (dataBytes[i]) & 0xff];
+            if ([hexStr length] == 2) {
+                [string appendString:hexStr];
+            } else {
+                [string appendFormat:@"0%@", hexStr];
+            }
+        }
+    }];
+    
+    return string;
+}
+
+
+
+
+@end
